b0c448be43dd68a3a38587b2a8433e54c023cea1
Anup Bhattachan <anupbhattachan10@gmail.com>
2026-07-21 15:51:29 +0545

    added logo in auth pages

--- client/src/layouts/AuthLayout.jsx
+++ client/src/layouts/AuthLayout.jsx
@@ -1,8 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 export default function AuthLayout() {
     return (
         <div>
+            <nav className="bg-slate-800 text-white p-4 shadow-lg">
+                <Link
+                    to="/"
+                    className="font-bold text-lg hover:text-sky-400 transition"
+                >
+                    📰 NewsApp
+                </Link>
+            </nav>
             <Outlet />
         </div>
     );
